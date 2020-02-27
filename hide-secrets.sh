babb79f904e1ac4430188ebf4fc9fa86da2e4230
Thomas Rodgers <thomasrodgers@google.com>
2020-02-27 12:03:30 -0800

    made org-id a number

--- hide-secrets.sh
+++ hide-secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/organization-id: \".*\"/organization-id: \"\"/" project-factory/core/*project.yaml
+sed -i "s/organization-id: .*/organization-id: /" project-factory/core/*project.yaml
 sed -i "s/external: \".*\"/external: \"\"/" project-factory/core/*project.yaml
