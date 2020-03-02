574ef7b232fed4df5eb76b3d906bad57c13419c5
Thomas Rodgers <thomasrodgers@google.com>
2020-03-02 14:08:07 -0800

    non-working unmarshal syntax

--- iam/pubsub/main.go
+++ iam/pubsub/main.go
@@ -1,12 +1,67 @@
 package main
 
 import (
-	"github.com/GoogleContainerTools/kpt-functions-sdk/go/pkg/framework/runners"
-	"kpt-samples/iam/pubsub/generator"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"sigs.k8s.io/kustomize/kyaml/kio"
+	"sigs.k8s.io/kustomize/kyaml/kio/filters"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
 const usage = "Generates IAM Policies for multiple pubsub topics."
 
 func main() {
-	runners.RunFunc(generator.Generate, usage)
+	fc, err := os.Open("fc.yaml")
+	if err != nil {
+		_ = fmt.Errorf("Error opening file: %v\n", err)
+	}
+	rw := &kio.ByteReadWriter{
+		Reader:                fc,
+		Writer:                os.Stdout,
+		KeepReaderAnnotations: true,
+	}
+	err = kio.Pipeline{
+		Inputs: []kio.Reader{rw},
+		Filters: []kio.Filter{
+			&filter{rw: rw},       // generate the Resources from the input
+			// set Resource filenames
+			&filters.FileSetter{FilenamePattern: filepath.Join("config", "%n.yaml")},
+			filters.FormatFilter{}, // format the output
+		},
+		Outputs: []kio.Writer{rw},
+	}.Execute()
+	if err != nil {
+		_ = fmt.Errorf("Pipeline error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+type filter struct {
+	rw *kio.ByteReadWriter
+}
+
+
+type PubSubIAMFunctionConfig struct {
+	Spec struct {
+		Project string `yaml:"project"`
+		PubSubTopics []string `yaml:"pubSubTopics"`
+		Bindings []Binding `yaml:"bindings"`
+	}
+}
+
+type Binding struct {
+	Role string `yaml:"role"`
+	Members []string `yaml:"members"`
+}
+
+func (f *filter) Filter(in []*yaml.RNode) ([]*yaml.RNode, error) {
+	var fc PubSubIAMFunctionConfig
+	bytes := []byte(f.rw.FunctionConfig.MustString())
+	err := yaml.Unmarshal(bytes, &fc)
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshalling function config: %v\n", err)
+	}
+	return in, err
 }
